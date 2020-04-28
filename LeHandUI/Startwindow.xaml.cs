169a8f9eb507ad4d2db563fc80ef3a530108a49c
Snotvlek <douwe@oscom.nl>
2020-04-28 17:02:01 +0200

    updated sum shit

--- LeHandUI/Startwindow.xaml.cs
+++ LeHandUI/Startwindow.xaml.cs
@@ -14,14 +14,45 @@ using System.Windows.Shapes;
 
 namespace LeHandUI
 {
-    /// <summary>
-    /// Interaction logic for Startwindow.xaml
-    /// </summary>
     public partial class Startwindow : Window
     {
         public Startwindow()
         {
             InitializeComponent();
+     
         }
-    }
+
+
+
+
+
+
+
+        //Handlers for custom titlebar buttons
+        #region TitleBarButtonHandlers
+        private void MinimizeWindow(object sender, EventArgs e)
+		{
+			App.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+		private void MaximizeWindow(object sender, EventArgs e)
+		{
+			if (App.Current.MainWindow.WindowState == WindowState.Maximized)
+			{
+				App.Current.MainWindow.WindowState = WindowState.Normal;
+			}
+			else if (App.Current.MainWindow.WindowState == WindowState.Normal)
+			{
+				App.Current.MainWindow.WindowState = WindowState.Maximized;
+			}
+		}
+		private void CloseWindow(object sender, EventArgs e)
+		{
+			Close();
+		}
+		private void DragStart(object sender, MouseButtonEventArgs e)
+		{
+			this.DragMove();
+		}
+		#endregion
+	}
 }
