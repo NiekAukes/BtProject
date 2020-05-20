e9d1933f14360ee806914621d2838a8c4971de71
Olivier Martin <olivier@labapart.com>
2020-05-20 10:58:43 +0200

    Update CommandManager.cpp

--- BtProject/CommandManager.cpp
+++ BtProject/CommandManager.cpp
@@ -228,8 +228,11 @@ void CommandManager::startcommander(bool intro, std::string loadfile)
 			std::string end;
 			//std::cin >> command;
 			if (*command.c_str() != '\0') {
-				command = command.substr(0, findinstr(command.c_str(), '\n') - 1);
-				end = command.substr(findinstr(command.c_str(), '\n') - 1, command.size());
+				int cmdsplit = findinstr(command.c_str(), '\n');
+				if (cmdsplit >= 0) {
+					end = command.substr(-1, command.size() - 1);
+					command = command.substr(0, findinstr(command.c_str(), '\n') - 1);
+				}
 
 
 
