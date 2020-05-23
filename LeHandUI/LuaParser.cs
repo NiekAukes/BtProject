34cf16e97a52afef3123a5fb2d8e0cfb65464550
Olivier Martin <olivier@labapart.com>
2020-05-23 13:51:41 +0200

    added parser

--- LeHandUI/LuaParser.cs
+++ LeHandUI/LuaParser.cs
@@ -6,20 +6,95 @@ using System.Threading.Tasks;
 
 namespace LeHandUI
 {
-    enum Condition
+    public abstract class LeHandAction
     {
-        Greater,
-        Less,
-        Equal
+        public abstract string Parse();
     }
-    struct Logic
+    public class Kpress : LeHandAction
     {
-        Condition condition;
-        double value;
+        char character = 'a';
 
-
+        public Kpress(char c)
+        {
+            character = c;
+        }
+        public override string Parse()
+        {
+            string s = "Kpress(\"" + character + "\")\n";
+            return s;
+        }
     }
-    class LuaParser
+    public class Mpress : LeHandAction
     {
+        short Button;
+        public override string Parse()
+        {
+            string s = "Kpress(" + Button + ")\n";
+            return s;
+        }
+    }
+    public class MMove : LeHandAction
+    {
+        double dX;
+        double dY;
+        public override string Parse()
+        {
+            string s = "Kpress(" + dX + ", " + dY + ")\n";
+            return s;
+        }
+    }
+    public class end : LeHandAction
+    {
+        public override string Parse()
+        {
+            return "return false\n";
+        }
+    }
+
+    public struct Logic
+    {
+        public Logic(int var, double begin, double end, LeHandAction Action)
+        {
+            variable = var;
+            beginrange = begin;
+            endrange = end;
+            action = Action;
+        }
+        public int variable;
+        public double beginrange, endrange;
+        public LeHandAction action;
+    }
+    public class LuaParser
+    {
+        public static readonly string[] varnames =
+        {
+            "LF", "RF", "MF", "IF", "Th", "AX", "AY", "AZ", "RY"
+        };
+
+        public static string Parse(Logic[] logics)
+        {
+            int nestvalue = 0;
+            string s = "LeHand = require \"LeHand\"\nUpdate = function ()\n";
+            nestvalue++;
+            for (int i = 0; i < logics.Length; i++)
+            {
+                Logic l = logics[i];
+                for (int j = 0; j < nestvalue; j++) { s += "\t"; }
+
+                s += "if " + varnames[l.variable] + " > " + l.beginrange.ToString("G", System.Globalization.CultureInfo.InvariantCulture) +
+                    " and " + varnames[l.variable] + " < " + l.endrange.ToString("G", System.Globalization.CultureInfo.InvariantCulture) + " then\n";
+                nestvalue++;
+
+                for (int j = 0; j < nestvalue; j++) { s += "\t"; }
+                s += l.action.Parse();
+                nestvalue--;
+
+                for (int j = 0; j < nestvalue; j++) { s += "\t"; }
+                s += "end\n\n";
+            }
+            nestvalue--;
+            s += "end\n";
+            return s;
+        }
     }
 }
